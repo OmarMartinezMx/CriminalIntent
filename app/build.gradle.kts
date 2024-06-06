6e5902a08da4d86840350ca54ef6e8cb704b33ec
Omar Martinez <a.omar.mtz@hotmail.com>
2024-06-05 22:35:44 -0700

    Chapter 10 - RecyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.recycler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
